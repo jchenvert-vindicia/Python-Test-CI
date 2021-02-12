c19e0d8f9978f4c2a8d27dc54e4696c025599019
Jonathon Chenvert <jchenvert@vindicia.com>
2021-02-12 15:37:49 -0800

    Pushing basic project to test with some GitHub-integrated CI

---
+++ test_math.py
@@ -0,0 +1,34 @@
+from basic_math import base_math
+
+import unittest
+
+
+class TestBaseMath(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(base_math.add(2,5), 7, "Should be 7")
+
+
+    def test_subtract(self):
+        self.assertEqual(base_math.subtract(5, 2), 3, "Should be 3")
+    
+    
+    def test_multiply(self):
+        self.assertEqual(base_math.multiply(5, 2), 10, "Should be 10")
+    
+    
+    def test_divide(self):
+        self.assertEqual(base_math.divide(10, 5), 2, "Should be 2")
+    
+    
+    def test_failing_add(self):
+        self.assertEqual(base_math.add(4, 4), 7, "Should be 8")
+
+
+def main():
+    unittest.main()
+
+
+if __name__ == "__main__":
+    main()
+
